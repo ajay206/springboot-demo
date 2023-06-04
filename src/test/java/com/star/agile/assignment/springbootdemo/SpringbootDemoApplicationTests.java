a3fcbe0a6ae83a97447c85f59c32aa47bcf3206a
Ajay206 <ajay206.k@gmail.com>
2023-06-04 11:22:17 +0530

    Initial commit

---
+++ src/test/java/com/star/agile/assignment/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.star.agile.assignment.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+
+}
